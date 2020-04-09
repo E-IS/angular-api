49d7616b894cce024ed6dfc8585b181cc5541905
Benoit Lavenier <benoit.lavenier@e-is.pro>
2020-04-09 12:30:00 +0200

    [enh] Allow to build artifact, using a gulp script

---
+++ release.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+
+# Get to the root project
+if [[ "_" == "_${PROJECT_DIR}" ]]; then
+  SCRIPT_DIR=$(dirname $0)
+  PROJECT_DIR=$(cd ${SCRIPT_DIR} && pwd)
+  export PROJECT_DIR
+fi;
+
+if [[ ! -f "${PROJECT_DIR}/package.json" ]]; then
+  echo "Invalid project dir: file 'package.json' not found in ${PROJECT_DIR}"
+  echo "-> Make sure to run the script inside his directory, or export env variable 'PROJECT_DIR'"
+  exit 1
+fi;
+
+echo "Preparing project environment.."
+PROJECT_NAME="angular-api"
+REPO="E-IS/angular-api"
+REPO_API_URL="https://api.github.com/repos/${REPO}"
+REPO_PUBLIC_URL="https://github.com/${REPO}"
+
+NODEJS_VERSION=10
+
+# Install node JS (using NVM)
+export NVM_DIR="$HOME/.nvm"
+if [[ -d "${NVM_DIR}" ]]; then
+
+    # Load NVM
+    . ${NVM_DIR}/nvm.sh
+
+    # Switch to expected version
+    nvm use ${NODEJS_VERSION}
+
+    # Or install it
+    if [[ $? -ne 0 ]]; then
+        nvm install ${NODEJS_VERSION}
+        if [[ $? -ne 0 ]]; then
+            exit 1;
+        fi
+    fi
+else
+    echo "nvm (Node version manager) not found (directory ${NVM_DIR} not found). Please install, and retry"
+    exit 1
+fi
+
+
+# Install project dependencies
+if [[ ! -d "${PROJECT_DIR}/node_modules" ]]; then
+    echo "Installing project dependencies..."
+    cd ${PROJECT_DIR}
+    yarn
+fi
+
+### Get current version (package.json)
+current=$(grep -oP "version\": \"\d+.\d+.\d+((a|b)[0-9]+)?" package.json | grep -m 1 -oP "\d+.\d+.\d+((a|b)[0-9]+)?")
+if [[ "_$current" == "_" ]]; then
+  echo "Unable to read the current version in 'package.json'. Please check version format is: x.y.z (x and y should be an integer)."
+  exit 1;
+fi
+echo "Current version: $current"
+
+
+# Check version format
+if [[ ! $2 =~ ^[0-9]+.[0-9]+.[0-9]+((a|b)[0-9]+)?$ ]]; then
+  echo "Wrong version format"
+  echo "Usage:"
+  echo " > ./release.sh [pre|rel] <version> <release_description>"
+  echo "with:"
+  echo " - pre: use for pre-release"
+  echo " - rel: for full release"
+  echo " - version: x.y.z"
+  echo " - release_description: a comment on release"
+  exit 1
+fi
+
+# Update the package version
+case "$1" in
+  rel|pre)
+    echo "new build version: $2"
+
+    # Change the version in files: 'package.json' and 'config.xml'
+    sed -i "s/version\": \"$current\"/version\": \"$2\"/g" package.json
+
+    ;;
+  *)
+    echo "No task given"
+    exit 1
+    ;;
+esac
+
+# Launch build
+gulp build --release
+[[ $? -ne 0 ]] && exit 1 # Stop if failed
+
